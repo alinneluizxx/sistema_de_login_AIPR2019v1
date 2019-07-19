f3da71f0b139fce496257ba01d1133a509578c36
Aline Luiza Gullich <48166592+alinneluizxx@users.noreply.github.com>
2019-07-19 14:48:21 -0300

    closes #2
    Formulário de cadastro de novo usuário

--- index.php
+++ index.php
@@ -50,9 +50,56 @@
                         <a href="#" id="btnCadastrar">Cadastre-se aqui.</a>
                         </p>
                     </div>
+                
                 </form>
             </div>
         </section>
+
+        <br>
+        <!-- Formulário de Cadastro -->
+        <section class="row mb-5">
+            <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaCadastro">
+                <h2 class="text-center">Cadastro de novo usuário</h2>
+                <form action="#" class="p-2" id="fromCadastro">
+                   
+                    <div class="form-group">
+                        <input type="text" name="nomeCompleto" id="nomeCompleto" class="form-control" placeholder="Nome Completo" required>
+                    </div>
+                    
+                    <div class="form-group">
+                        <input type="text" name="nomeUsuário" id="nomeUsuário" class="form-control" placeholder="Nome de Usuário" required>
+                    </div>
+                    
+                    <div class="form-group">
+                        <input type="email" name="emailUsuário" id="emailUsuário" class="form-control" placeholder="E-mail de Usuário" required>
+                    </div>
+                    
+                    <div class="form-group">
+                        <input type="password" name="senhaUsuário" id="senhaUsuário" class="form-control" placeholder="Digite sua senha" required>
+                    </div>
+                    
+                    <div class="form-group">
+                    <input type="password" name="senhaConfirma" id="senhaConfirma" class="form-control" placeholder="Confirme sua senha" required>
+                    </div>
+                    
+                    <div class="form-group">
+                        <div class="custom-control custom-checkbox">
+                            <input type="checkbox" name="concordar" id="concordar" class="custom-control-input">
+                            <label for="concordar" class="custom-control-label">Eu concordo com os <a href="#">termos e condições.</a></label>
+                        </div>
+                    </div>
+                    
+                    <div class="form-group">
+                        <input type="submit" value=":: Cadastrar ::" class="btn btn-primary btn-block" id="btnRegistrar">
+                    </div>
+                    
+                    <div class="form-group">
+                        <p class="text-center">Já cadastrado?<a href="#" id="btnJáCadastrado">Entrar aqui.</a></p>
+                    </div>
+                </form>
+            </div>
+        </section>
+    
     </main>
 
 
