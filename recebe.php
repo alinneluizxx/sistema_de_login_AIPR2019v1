920d826e6e7c33b9dd84d9429122bde5acf9d770
Aline Luiza Gullich <aline_gullich@estudante.sc.senai.br>
2019-08-15 16:20:20 -0300

    Ajax envio dos dados para back-end

--- recebe.php
+++ recebe.php
@@ -1,9 +1,9 @@
 <?php 
-#Backend
+/** 
+ * aula04 15/08/2019 - Recuperação de dados do Back-end 
+*/
+#Back-end
 
 echo "<pre>\n";
-print_r($_GET);
+print_r($_POST);
 echo "</pre>\n";
-
-echo "<h1>Nome: $_GET[usuario]</h1>";
-echo "<h2>Senha: $_GET[senha]</h2>";
