7bc1f6e9a934777f566920598b0137ec1e78e102
Aline Luiza Gullich <aline_gullich@estudante.sc.senai.br>
2019-08-30 16:31:46 -0300

    Cadastro de Usuário
    closes #8

--- recebe.php
+++ recebe.php
@@ -1,43 +1,56 @@
-<?php 
+<?php
 
 //É necessário fazer a conexão com o Banco de Dados
-require_once"configDB.php";
+require_once "configDB.php";
 
-function verificar_entrada($entrada){
+function verificar_entrada($entrada)
+{
     $saida = trim($entrada); //Remove espaços antes e depois
     $saida = stripslashes($saida); //Remove as barras
     $saida = htmlspecialchars($saida);
     return $saida;
 }
 
-if(isset($_POST['action']) && $_POST['action'] == 'cadastro'){
+if (isset($_POST['action']) && $_POST['action'] == 'cadastro') {
     $nomeCompleto = verificar_entrada($_POST['nomeCompleto']);
     $nomeUsuário = verificar_entrada($_POST['nomeUsuário']);
     $emailUsuário = verificar_entrada($_POST['emailUsuário']);
     $senhaUsuário = verificar_entrada($_POST['senhaUsuário']);
-    $senhaConfirma = verificar_entrada ($_POST['senhaConfirma']);
+    $senhaConfirma = verificar_entrada($_POST['senhaConfirma']);
     $concordar = $_POST['concordar'];
     $dataCriacao = date("Y-m-d H:i:s");
 
-    echo "<h5>Nome completo: $nomeCompleto</h5>";
-    echo "<h5>Nome Usuário: $nomeUsuário</h5>";
-    echo "<h5>E-mail Usuário: $emailUsuário</h5>";
-    echo "<h5>Senha Usuário: $senhaUsuário</h5>";
-    echo "<h5>Senha Confirma: $senhaConfirma</h5>";
-    echo "<h5>Concordar: $concordar</h5>";
-    echo "<h5>Data de Criação: $dataCriacao</h5>";
 
     //Hash de senha / Codificação de senha em 40 caracteres
     $senha = sha1($senhaUsuário);
     $senhaC = sha1($senhaConfirma);
 
-    if($senha != $senhaC){
+    if ($senha != $senhaC) {
         echo "<h1>As senhas não conferem</h1>";
         exit();
-    }else{
-        echo "<h5> senha codificada : $senha</h5>";
+    } else {
+        //echo "<h5> senha codificada : $senha</h5>";
+        //Verificar se o usuário já existe no banco de dados
+        $sql = $conecta->prepare("SELECT nomeUsuario, email FROM usuario WHERE nomeUsuario = ? OR email = ?");
+        //Substitui cada ? por uma string abaixo
+        $sql->bind_param("ss", $nomeUsuario, $emailUsuario);
+        $sql->execute();
+        $resultado = $sql->get_result();
+        $linha = $resultado->fetch_array(MYSQLI_ASSOC);
+        if ($linha['nomeUsuario'] == $nomeUsuario) {
+            echo "<p>Nome de usuário indisponível, tente outro</p>";
+        } elseif ($linha['email'] == $emailUsuario) {
+            echo "<p>E-mail já em uso, tente outro</p>";
+        } else {
+            $sql = $conecta->prepare("INSERT into usuario (nome, nomeUsuario, email, senha, dataCriacao) values(?, ?, ?, ?, ?)");
+            $sql->bind_param("sssss", $nomeCompleto, $nomeUsuario, $emailUsuario, $senha, $dataCriacao);
+            if ($sql->execute()) {
+                echo "<p>Registrado com sucesso</p>";
+            } else {
+                echo "<p>Algo deu errado. Tente outra vez.</p>";
+            }
+        }
     }
-
-}else{
+} else {
     echo "<h1 style='color:red'>Esta página não pode ser acessada diretamente</h1>";
 }
